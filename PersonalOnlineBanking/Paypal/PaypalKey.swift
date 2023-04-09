42de8c2818821dcd29b59dacf304891d074ccfd0
shadowfsdev <118349590+shadowfsdev@users.noreply.github.com>
2023-04-10 03:22:41 +1000

    Add files via upload

---
+++ PersonalOnlineBanking/Paypal/PaypalKey.swift
@@ -0,0 +1,14 @@
+//
+//  PaypalKey.swift
+//  PersonalOnlineBanking
+//
+//  Created by Abu Nabe on 9/4/2023.
+//
+
+import UIKit
+
+class PaypalKey{
+    static var clientID = "AaC2y8dOx3dYKgtztfUFC-wgv_BAvv_Icq57va62icLv4zwSj1dSdbOYHfcUgRRAt8zI3FvM6QBCC0u1"
+    
+    static var secretID = "EJ8AbBwqeNMvH1csQBLbdoWTCWs0tLgR42LyisTyGs414pica4B0KPnAkpLRDFYSOc3NMZVO4otP9wt3"
+}
