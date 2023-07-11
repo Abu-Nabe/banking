6e5ba12a1ba94aa14608b84aeede2de96ab537a2
Abu <118349590+shadowfsdev@users.noreply.github.com>
2023-07-12 03:49:42 +1000

    Update StripeConfig.swift

--- PersonalOnlineBanking/Stripe/StripeConfig.swift
+++ PersonalOnlineBanking/Stripe/StripeConfig.swift
@@ -11,6 +11,6 @@ import Stripe
 class StripeConfig{
     static func config(){
         let config = STPPaymentConfiguration.shared
-        config.publishableKey = "pk_test_51M5o3IGWslu84Leou5Mvu4ISGxHaLIUAabIEDKs6m1x0MhVoMzQgiTj5G5OjLMoX5RT4vTuRzGghsqhrzcZbLErh00rF9gztkO"
+        config.publishableKey = // removed
     }
 }
